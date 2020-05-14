4a6043b5abd92499c60cf641361b1b2b90b2dbae
Jon Sibala <jsibala@covisus.com>
2020-05-14 15:14:47 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jon Sibala on 5/14/20.
+//  Copyright © 2020 Jon Sibala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
